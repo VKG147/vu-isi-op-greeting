b1566178a809808d582f372866a2dc93d77c8209
Vladas Zvonkus <vladas.zvonkus@gmail.com>
2020-02-07 12:46:48 +0200

    Add setRows function

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 
 void getInput(std::string& name);
+void setRows(std::string rows[3], std::string name);
 
 int main()
 {
     std::string name;
-    std::string rows[5];
+    std::string rows[3];
 
     getInput(name);
+
+    setRows(rows, name);
 
     return 0;
 }
@@ -15,4 +18,18 @@ int main()
 void getInput(std::string& name)
 {
     std::cin >> name;
+}
+
+void setRows(std::string rows[3], std::string name)
+{
+    rows[2] = "* Sveikas, " + name + "! *";
+
+    rows[0] = "*";
+    rows[1] = "*";
+    for (int i = 0; i < rows[2].length() - 2; ++i) {
+        rows[0] += "*";
+        rows[1] += " ";
+    }
+    rows[0] += "*";
+    rows[1] += "*";
 }
