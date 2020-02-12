bc300e264ac62b1c456727f0df8d0ab0d230df4f
Vladas Zvonkus <vladas.zvonkus@gmail.com>
2020-02-12 11:17:17 +0200

    Implement choosable frame size

--- main.cpp
+++ main.cpp
@@ -1,27 +1,34 @@
 #include <iostream>
 
-int getInput(std::string& name);
+int getInput(std::string& name, int& row_count);
 void setRows(std::string rows[3], std::string name);
-void printGreeting(std::string name);
+void printGreeting(std::string name, int row_count);
 bool isMale(std::string name);
 
 int main()
 {
     std::string name;
+    int row_count;
 
-    std::cout << "Please enter your name: ";
-    getInput(name);
+    getInput(name, row_count);
 
-    printGreeting(name);
+    printGreeting(name, row_count);
 
     return 0;
 }
 
-int getInput(std::string& name)
+int getInput(std::string& name, int& row_count)
 {
-    if (std::cin >> name) {
-        return 1;
-    }
+    std::cout << "Please enter your name: " << std::endl;
+    std::cin >> name;
+
+    do {
+        std::cout << "Please enter the row count of the frame (odd number):" << std::endl;
+        std::cin >> row_count;
+    } while(!std::cin || row_count < 3 || row_count > 150);
+
+    if (row_count % 2 == 0) row_count--;
+
     return 0;
 }
 
@@ -44,16 +51,24 @@ void setRows(std::string rows[3], std::string name)
     rows[1] += "*";
 }
 
-void printGreeting(std::string name)
+void printGreeting(std::string name, int row_count)
 {
     std::string rows[3];
     setRows(rows, name);
 
-    std::cout << rows[0] << std::endl
-              << rows[1] << std::endl
-              << rows[2] << std::endl
-              << rows[1] << std::endl
-              << rows[0] << std::endl;
+    std::cout << rows[0] << std::endl;
+
+    for (int i = 0; i < (row_count - 3)/2; ++i) {
+        std::cout << rows[1] << std::endl;
+    }
+
+    std::cout << rows[2] << std::endl;
+
+    for (int i = 0; i < (row_count - 3)/2; ++i) {
+        std::cout << rows[1] << std::endl;
+    }
+
+    std::cout << rows[0] << std::endl;
 }
 
 bool isMale(std::string name)
