7820a7272a20407db67f93542b37ca72626dfe84
Vladas Zvonkus <vladas.zvonkus@gmail.com>
2020-03-05 17:37:16 +0200

    AAA

--- main.cpp
+++ main.cpp
@@ -4,15 +4,11 @@ int getInput(std::string& name, int& row_count);
 void setRows(std::string rows[3], std::string name);
 void printGreeting(std::string name, int row_count);
 bool isMale(std::string name);
-
-int main()
-{
+jlvfdg
     std::string name;
     int row_count;
 
-    getInput(name, row_count);
-
-    printGreeting(name, row_count);
+    getInm ln, row_count);
 
     return 0;
 }
@@ -24,23 +20,7 @@ int getInput(std::string& name, int& row_count)
 
     do {
         std::cout << "Please enter the HAHA RANDOM UNEXPECTED CHANGE of the frame (odd number):" << std::endl;
-        std::cin >> row_count;
-    } while(!std::cin || row_count < 3 || row_count > 150);
-
-    if (row_count % 2 == 0) row_count--;
-
-    return 0;
-}
-
-void setRows(std::string rows[3], std::string name)
-{
-    if (isMale(name)) {
-        rows[2] = "* Sveikas, " + name + "! *";
-    }
-    else {
-        rows[2] = "* Sveika, " + name + "! *";
-    }
-
+        std::cin >> row_countkdmssfnosndjs
     rows[0] = "*";
     rows[1] = "*";
     for (int i = 0; i < rows[2].length() - 2; ++i) {
@@ -50,16 +30,7 @@ void setRows(std::string rows[3], std::string name)
     rows[0] += "*";
     rows[1] += "*";
 }
-
-void printGreeting(std::string name, int row_count)
-{
-    std::string rows[3];
-    setRows(rows, name);
-
-    std::cout << rows[0] << std::endl;
-
-    for (int i = 0; i < (row_count - 3)/2; ++i) {
-        std::cout << rows[1] << std::endl;
+    njcdsks] << std::endl;
     }
 
     std::cout << rows[2] << std::endl;
@@ -74,7 +45,4 @@ void printGreeting(std::string name, int row_count)
 bool isMale(std::string name)
 {
     if (name[name.length()-1] == 's' || name[name.length()-1] == 'S') {
-        return true;
-    }
-    return false;
-}
+        retljvdhu
